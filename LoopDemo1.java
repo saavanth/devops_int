d79bb556fb616469c74390cf36a8b755394db334
saavanth <159518574+saavanth@users.noreply.github.com>
2025-07-11 12:47:06 +0530

    Add files via upload

---
+++ LoopDemo1.java
@@ -0,0 +1,15 @@
+class LoopDemo1
+{
+public static void main(String args[])
+{
+int sum=0,avg;
+for(int i=0;i<=10;i++)
+{
+sum=sum+i;
+}
+avg=sum/10;
+System.out.println("Sum ="+sum);
+System.out.println("Average ="+avg);
+}
+}
+
